dcd923d5b78a84cb540e33fdf6909ce17dd1ccf8
Sebastián Pineda <spineda0424@gmail.com>
2023-02-26 11:20:49 -0500

    Creación del proyecto

---
+++ src/main/java/pineda/sebastian/examsapirest/ExamsApirestApplication.java
@@ -0,0 +1,13 @@
+package pineda.sebastian.examsapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pineda/sebastian/examsapirest/ExamsApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package pineda.sebastian.examsapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
